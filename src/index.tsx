7baa4cd7c3b5290f6b5492bcb673ea87188025d6
Alex Merced <alex@alexmerced.dev>
2022-05-15 08:26:35 -0400

    react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./styles.scss";
+import { createRoot, Root } from "react-dom/client";
 
 interface AppProps {
   hello: String;
@@ -10,4 +11,5 @@ const App: React.FC<AppProps> = (props) => {
   return <h1>{props.hello}</h1>;
 };
 
-ReactDom.render(<App hello="hello world"/>, document.querySelector("#root"));
+const root:Root = createRoot(document.querySelector("#root"));
+root.render(<App hello="hello world"/>)
